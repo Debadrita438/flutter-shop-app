41da3355471e38d990d9240db5e23bc84122f6a7
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2022-09-23 16:52:59 +0530

    added provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -7,6 +7,9 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productId =
+        ModalRoute.of(context)?.settings.arguments as String; // is the id!
+
     return Scaffold(
       appBar: AppBar(
         title: Text('title'),
