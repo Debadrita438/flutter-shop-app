c2b6d0ff2b5992e93529901e4b81530c05364c02
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2022-11-07 17:39:59 +0530

    added fetch order using future builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -15,36 +15,48 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+  late Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<OrderProvider>(context, listen: false).fetchOrders();
+  }
 
   @override
   void initState() {
-    // Future.delayed(Duration.zero).then((_) async {
-    _isLoading = true;
-    Provider.of<OrderProvider>(context, listen: false)
-        .fetchOrders()
-        .then((_) => setState(() => _isLoading = false));
-
-    // });
+    _ordersFuture = _obtainOrdersFuture();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<OrderProvider>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Orders'),
       ),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: _ordersFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, i) => OrderItems(order: orderData.orders[i]),
-            ),
+            );
+          } else {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Consumer<OrderProvider>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) =>
+                      OrderItems(order: orderData.orders[i]),
+                ),
+              );
+            } else {
+              return Center(
+                child: Text('An error occured!'),
+              );
+            }
+          }
+        },
+      ),
       drawer: AppDrawer(),
     );
   }
